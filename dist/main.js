4fa84d4f47c8cb935f4f636970e08e49130aa2c2
root <root@DESKTOP-FC6JEE7.localdomain>
2022-03-21 16:13:05 +0100

    first commit

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let a = 12;
+a += 23;
+console.log("la variabile 'a' ha un valore di " + a);
+let myInterface = {
+    myNumber: 27,
+    myString: "Maggio"
+};
+console.log("My number= ", myInterface.myNumber);
+console.log("My string= ", myInterface.myString);
